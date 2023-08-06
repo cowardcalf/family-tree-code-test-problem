f482ebc0b8627999bbfec83b026ac9f07087ad08
Cowardcalf <cowardcalf@gmail.com>
2023-08-06 23:46:51 +1000

    Finish the QuestionOne functions.

--- src/components/questions/QuestionOne.tsx
+++ src/components/questions/QuestionOne.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import QuestionContainer from "../questionContainer/QuestionContainer";
 import { problems } from "../../constants/problems";
-import QuestionInput from "../questionInput/QuestionInput";
 import QuestionListInput from "../questionListInput/QuestionListInput";
-import { RelationsArray } from "../../types/relation";
+import { Relation, RelationsArray } from "../../types/relation";
 import { PersonMap } from "../../types/personMap";
+import { findPersonOfRelation } from "../../utils/findPersonOfRelation";
 
 type QuestionOneProps = {
   personMap: PersonMap;
@@ -13,15 +13,29 @@ type QuestionOneProps = {
 const QuestionOne = ({ personMap }: QuestionOneProps) => {
   const [person, setPerson] = useState("");
   const [relation, setRelation] = useState("");
-  const [result, setResult] = useState<string[]>([]);
+  const [result, setResult] = useState<string[] | null>(null);
+
+  const personNames = Object.keys(personMap);
+
+  const clearResult = () => setResult(null);
+
+  const onPersonChange = (newValue: string) => {
+    clearResult();
+    setPerson(newValue);
+  };
+
+  const onRelationChange = (newValue: string) => {
+    clearResult();
+    setRelation(newValue);
+  };
 
   const inputs = [
-    <QuestionInput key="q1Person" title="Person" value={person} onChange={setPerson} />,
+    <QuestionListInput key="q1Person" title="Person" value={person} onChange={onPersonChange} options={personNames} />,
     <QuestionListInput
       key="q1Relation"
       title="Relation"
       value={relation}
-      onChange={setRelation}
+      onChange={onRelationChange}
       options={RelationsArray}
     />
   ];
@@ -31,12 +45,18 @@ const QuestionOne = ({ personMap }: QuestionOneProps) => {
       alert("Please input a valid person and relation");
     } else if (!personMap[person]) {
       alert("There is no this person");
+    } else if (!Object.values(Relation).includes(relation as Relation)) {
+      alert("There is no such relation");
     } else {
-      setResult([person, relation]);
+      setResult(findPersonOfRelation(person, relation as Relation, personMap));
     }
   };
 
-  const resultStr = result.join(", ");
+  const resultStr = result
+    ? result.length > 0
+      ? `Result: ${person}'s ${relation} ${result.length > 1 ? "are" : "is"} ${result.join(", ")}`
+      : `Result: ${person} has no ${relation}`
+    : "";
 
   return <QuestionContainer title={problems[0]} inputs={inputs} onEnter={onEnter} result={resultStr} />;
 };
